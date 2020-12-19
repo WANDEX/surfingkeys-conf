34842296e1e772331af7b1322254f68d26ff541d
WANDEX <downloadfils@gmail.com>
2020-12-19 19:30:47 +0300

    .editorconfig format

--- gulpfile.js
+++ gulpfile.js
@@ -12,21 +12,21 @@ const path = require("path")
 const platforms = require("platform-folders")
 
 const paths = {
-    entry:            "src/main.js",
-    gulpfile:         "gulpfile.js",
-    scriptOut:        "surfingkeys.js",
-    installDir:       path.join(platforms.getConfigHome(), "surfingkeys"),
+  entry:            "src/main.js",
+  gulpfile:         "gulpfile.js",
+  scriptOut:        "surfingkeys.js",
+  installDir:       path.join(platforms.getConfigHome(), "surfingkeys"),
 }
 
 task("clean", () => del(["build", ".cache", ".tmp-gulp-compile-*"]))
 
 task('build', () => src(paths.entry, { read: false })
-    .pipe(parcel())
-    .pipe(rename(paths.scriptOut))
-    .pipe(dest("build"))
+  .pipe(parcel())
+  .pipe(rename(paths.scriptOut))
+  .pipe(dest("build"))
 )
 
 task("install", series("build", () => src(path.join("build", paths.scriptOut))
-    .pipe(dest(paths.installDir))))
+  .pipe(dest(paths.installDir))))
 
 task('default', series('build'))
