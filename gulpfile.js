3fd39977125c6db4649a4cdf6257e5a3cce0dc16
WANDEX <downloadfils@gmail.com>
2021-04-24 22:58:01 +0300

    removed npm packages: gulp-notify, plugin-error

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp")
 
 // removes unnecessary repetition
 const { task, src, dest } = gulp
-const { parallel, series } = gulp
+const { series } = gulp
 
 const webpack = require("webpack")
 const webpackStream = require("webpack-stream")
@@ -11,10 +11,7 @@ const rename = require("gulp-rename")
 const del = require("del")
 const path = require("path")
 const platforms = require("platform-folders")
-const gulpNotify = require("gulp-notify")
-//const PluginError = require("plugin-error")
 
-const pkg = require("./package.json")
 const webpackConfig = require("./webpack.config.js")
 
 const paths = {
@@ -25,44 +22,13 @@ const paths = {
   installDir: path.join(platforms.getConfigHome(), "surfingkeys"),
 }
 
-const notify = Object.assign((opts, ...args) => gulpNotify({
-  icon:    null,
-  onLast:  true,
-  timeout: 3000,
-  ...opts,
-}, ...args), gulpNotify)
-
-notify.onError = (opts, ...args) => gulpNotify.onError({
-  onLast:  true,
-  timeout: 7500,
-  ...opts,
-}, ...args)
-
 task("clean", () => del(["build", ".cache", ".tmp-gulp-compile-*"]))
 
 const build = () =>
   src(paths.entry)
     .pipe(webpackStream(webpackConfig, webpack))
-    .on("error", (err) => {
-      notify.onError({
-        title:   `[gulp] Build failure [${pkg.name}]`,
-        message: `${err.message.split("\n").slice(0, 1)}`,
-      })(err)
-      throw err
-    })
     .pipe(rename(paths.scriptOut))
     .pipe(dest(paths.buildDir))
-    .pipe(notify({
-      title:   `[gulp] Build success [${pkg.name}]`,
-      message: "No issues",
-    }))
-
-/*task("build",
-  parallel(
-    "lint",
-    build,
-  ))
-*/
 
 task("build", series(build))
 
